a51cae8579a318838090a916b4a50e5080321b1c
dantemccflurry <dmccurty@ucsd.edu>
2023-11-28 12:37:16 -0800

    This is my autograder that WONT WORK

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,45 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+files=`find . -name "*.java" `
+for file in $files 
+do
+    if [[ -f $file ]] && [[ $file == **/ListExamples.java* ]]
+    then 
+        echo "file found"
+        cp $file grading-area
+    fi
+done
+
+cp TestListExamples.java grading-area
+
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+if [[ $? != 0 ]]
+    then 
+        echo compilation error
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test.txt
+
+
+cd ..
+grep -Eo run:[0-9]+ grading-area/test.txt > Tests.txt
+
+numTests=`grep -Eo [0-9]+ Tests.txt`
+
+echo $numTests
+
+grep -Eo Failiure:[0-9]+ grading-area/test.txt > Failures.txt
+
+numFail=`grep -Eo [0-9]+ Failures.txt`
+
+echo $numFail
 
 
 # Draw a picture/take notes on the directory structure that's set up after
